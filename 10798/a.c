e0f26056a8b85fef6da2bf796dd7d29ee5ccb83b
pjuhyeon <qkrwngus3563@naver.com>
2025-02-05 04:45:11 +0900

    10798

---
+++ 10798/a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char arr[5][15] = {'\0', };
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%s", arr[i]);
+    }
+
+    for (int i = 0; i < 15; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (arr[j][i] != '\0')
+            {
+                printf("%c", arr[j][i]);
+            }
+        }
+    }
+}
