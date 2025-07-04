0f377c7cb45127e0a0a1c8bf9c0c43d576a739f2
zoo <qkrwngus3563@naver.com>
2025-07-04 18:52:49 +0900

    1000

--- BAEKJOON/1000s/15894/a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    long long int n;
-    scanf("%lld", &n);
-    printf("%lld", n * 4);
-}
--- BAEKJOON/2000s/24262/a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("1\n0");
-
-    return 0;
-}
