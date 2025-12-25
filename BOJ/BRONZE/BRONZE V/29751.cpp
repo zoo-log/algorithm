57fdc93c4def6856f91f00eef519ed66d4ae8f6a
zoo-log <qkrwngus3563@naver.com>
2025-12-25 05:16:50 +0000

    BRONZE V 삼각형

---
+++ BOJ/BRONZE/BRONZE V/29751.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main(void)
+{
+
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
+    float W, H;
+    std::cin >> W >> H;
+
+    float A = W * H / 2;
+    printf("%.1f",A);
+
+    return 0;
+}
